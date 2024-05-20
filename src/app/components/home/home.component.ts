a03fde7e1a507055568658d14bf1d408294a754f
ghazal <ghazalfatal.02@gmail.com>
2024-05-20 23:27:32 +0300

    adding the about us compoonent to the landing page and the menu component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { MenuComponent } from '../menu/menu.component';
 import { FooterComponent } from '../footer/footer.component';
-
+import { AboutComponent } from '../about/about.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent,MenuComponent,FooterComponent],
+  imports: [NavbarComponent,MenuComponent,FooterComponent,AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
